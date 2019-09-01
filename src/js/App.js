2878ce961085a1adbf252f6405e6c8ccdea7fd73
Julian Smith <juliansmith@Julians-MacBook-Pro.local>
2019-09-01 14:23:07 +0100

    Added form validation

--- src/js/App.js
+++ src/js/App.js
@@ -18,7 +18,43 @@ class App extends Component {
         email: "",
         password: "",
         updatesPrimary: true,
-        updatedThird: false
+        updatedThird: false,
+        inputsValid: {
+            name: false,
+            email: false,
+            password: false
+        }
+    };
+
+    // Function which determins if the form is valid or not
+    formValid = () =>
+        this.state.inputsValid.name &&
+        this.state.inputsValid.email &&
+        this.state.inputsValid.password;
+
+    // Function which validates the required inputs
+    validateInput = (name, value) => {
+        let errors = this.state.inputsValid;
+        // RFC 2822 email validation -  matches "99.99% of all email addresses in actual use today.". Source = https://www.regular-expressions.info/email.html
+        const validEmail = RegExp(
+            /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+        );
+        // If password has 1 number, 1 lowercase letter, 1 uppercase letter
+        const validPassword = RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*/g);
+
+        switch (name) {
+            case "name":
+                errors.name = value.length > 0;
+                break;
+            case "email":
+                errors.email = validEmail.test(value);
+                break;
+            case "password":
+                errors.password = value.length > 9 && validPassword.test(value);
+                break;
+            default:
+                break;
+        }
     };
 
     // Calls when an input's value has changed
@@ -30,6 +66,10 @@ class App extends Component {
             target.type === "checkbox" ? target.checked : target.value;
         // Get the input's name
         const name = target.name;
+
+        // Validation
+        this.validateInput(name, value);
+
         // Update the corresponding input's state with the new value
         this.setState({
             [name]: value
@@ -38,28 +78,33 @@ class App extends Component {
 
     // Calls when the form is submited
     handleSubmit = e => {
-        // Next page
-        this.nextPage(e, "privacy", "done");
-        // Get state of inputs
-        const {
-            name,
-            role,
-            email,
-            password,
-            updatesPrimary,
-            updatedThird
-        } = this.state;
-        // Create object with the value of all the inputs
-        const formData = {
-            name,
-            role,
-            email,
-            password,
-            updatesPrimary,
-            updatedThird
-        };
-        // Output object to console as JSON
-        console.log(JSON.stringify(formData));
+        // Prevent page from reloading
+        e.preventDefault();
+        // Is the form valid?
+        if (this.formValid()) {
+            // Next page
+            this.nextPage(e, "privacy", "done");
+            // Get state of inputs
+            const {
+                name,
+                role,
+                email,
+                password,
+                updatesPrimary,
+                updatedThird
+            } = this.state;
+            // Create object with the value of all the inputs
+            const formData = {
+                name,
+                role,
+                email,
+                password,
+                updatesPrimary,
+                updatedThird
+            };
+            // Output object to console as JSON
+            console.log(JSON.stringify(formData));
+        }
     };
 
     nextPage = (e, currentPage, nextPage) => {
@@ -83,12 +128,18 @@ class App extends Component {
                 <div className="form-area">
                     {/* Show Page Display */}
                     <PageDisplay pages={this.state.pages} />
-                    <form className="form" onSubmit={this.handleSubmit}>
+                    <form
+                        className="form"
+                        onSubmit={this.handleSubmit}
+                        noValidate
+                    >
                         {/* Display "User" Page */}
                         {this.state.pages.user && (
                             <User
                                 handleChange={this.handleChange}
                                 nextPage={this.nextPage}
+                                errors={this.state.inputsValid}
+                                noErrors={this.formValid()}
                             />
                         )}
                         {/* Display "Privacy" Page */}
--- src/js/User.js
+++ src/js/User.js
@@ -1,30 +1,88 @@
-import React from "react";
+import React, { Component } from "react";
 import "../css/User.css";
 
-function InputField({ name, type, text, required, change }) {
-    return (
-        <div className="input-field">
-            <label>
-                {text}
-                {required && <span className="required">*</span>}
-                <input
-                    name={name}
-                    type={type}
-                    required={required}
-                    onChange={change}
-                ></input>
-            </label>
-        </div>
-    );
+class InputField extends Component {
+    state = {
+        clicked: false
+    };
+
+    // Update the state to display the error message once the field has changed value
+    showError = () => {
+        this.setState({ clicked: true });
+    };
+
+    render() {
+        // Streamline the props variables
+        const { name, type, text, required, errors, change } = this.props;
+        return (
+            <div className="input-field">
+                <label>
+                    {text}
+                    {required && <span className="required">*</span>}
+                    <input
+                        name={name}
+                        type={type}
+                        required={required}
+                        onChange={e => {
+                            this.showError();
+                            change(e);
+                        }}
+                        noValidate
+                    ></input>
+                    <div className="error-area">
+                        {errors && (
+                            <div
+                                className={`error${
+                                    this.state.clicked && !errors.valid
+                                        ? " show"
+                                        : ""
+                                }`}
+                            >
+                                {errors.text}
+                            </div>
+                        )}
+                    </div>
+                </label>
+            </div>
+        );
+    }
 }
 
-function User({ handleChange, nextPage }) {
+function User({ handleChange, nextPage, errors, noErrors }) {
     // Generate an array of all the required inputs
     const inputsData = [
-        { name: "name", type: "text", text: "Name", required: true },
+        {
+            name: "name",
+            type: "text",
+            text: "Name",
+            required: true,
+            error: {
+                valid: errors.name,
+                text: "Please enter your name"
+            }
+        },
         { name: "role", type: "text", text: "Role", required: false },
-        { name: "email", type: "email", text: "Email", required: true },
-        { name: "password", type: "password", text: "Password", required: true }
+        {
+            name: "email",
+            type: "email",
+            text: "Email",
+            required: true,
+            error: {
+                valid: errors.email,
+                text: "Please enter a valid email"
+            }
+        },
+        {
+            name: "password",
+            type: "password",
+            text: "Password",
+            required: true,
+            error: {
+                valid: errors.password,
+                text:
+                    "Password must be at least 10 characters long, have at least 1 number, and must have at least 1 capital and lowercase letter"
+            }
+        }
     ];
     // Map over the array and for each item in the array create an input
     const inputs = inputsData.map((input, i) => (
@@ -34,6 +92,7 @@ function User({ handleChange, nextPage }) {
             type={input.type}
             text={input.text}
             required={input.required}
+            errors={input.error}
             change={handleChange}
         />
     ));
@@ -42,8 +101,8 @@ function User({ handleChange, nextPage }) {
             {inputs}
             <div className="page-btn-area">
                 <button
-                    className="page-btn"
-                    onClick={e => nextPage(e, "user", "privacy")}
+                    className={`page-btn${noErrors ? " valid" : ""}`}
+                    onClick={e => noErrors && nextPage(e, "user", "privacy")}
                 >
                     Next
                 </button>
